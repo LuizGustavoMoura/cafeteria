a52fee3dccedebca7ae4e11e541d47e01548cd68
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-02-06 20:46:39 -0300

    criação do formulário de login e regisro

---
+++ conexao.php
@@ -0,0 +1,8 @@
+<?php
+$hostname='localhost';
+$username='root';
+$password='';
+$database='cafeteria';
+$port='3307';
+$conexao = mysqli_connect($hostname, $username, $password, $database, $port);
+?>
