35c034bf6865b6b8ab6cff91cd44a8420e1bef4f
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-02-08 20:27:22 -0300

    Erro de conexão corrigido no servidor MySQL (o servidor MySQL queria a porta 3307 e não a porta 3306)

--- conexao.php
+++ conexao.php
@@ -3,6 +3,12 @@ $hostname='localhost';
 $username='root';
 $password='';
 $database='cafeteria';
-$port='3306';
+$port='3307';
+// Tente estabelecer a conexão
 $conexao = mysqli_connect($hostname, $username, $password, $database, $port);
+
+// Verifique se a conexão foi bem-sucedida
+if (!$conexao) {
+    die('Erro na conexão: ' . mysqli_connect_error());
+}
 ?>
