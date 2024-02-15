e4badf834a01c882fcf7940e69c1fef09bba645d
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-02-15 20:08:31 -0300

    criação do formulário do corpo do modal "Editar"

--- formcliente.php
+++ formcliente.php
@@ -214,11 +214,11 @@
 													<div class="modal-content">
 														<div class="modal-header">
 															<h5 class="modal-title" id="modalTitleId">
-																Modal title
+																Deseja realmente excluir?
 															</h5>
 															<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 														</div>
-														<div class="modal-body">Deseja realmente excluir?</div>
+														<div class="modal-body"></div>
 														<div class="modal-footer">
 															<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
 																Voltar
